d607d57acfb383297f89d67d203c9f4d58cf9c77
akash-912 <akashdeep12896@gmail.com>
2025-12-22 21:56:13 +0530

    user management

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,25 @@
 import express from "express";
 
 // controllers
-import {createUser} from "../controllers/userController.js"
+import {
+  createUser,
+  loginUser,
+  logCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile
+} from "../controllers/userController.js"
 // middlewares
-
+import { authenticate,authorizeAdmin } from "../middlewares/authMiddleware.js";
 const userRoutes = express.Router()
 
-userRoutes.route('/').post(createUser);
+userRoutes.route('/').post(createUser).get(authenticate,authorizeAdmin, getAllUsers);
+userRoutes.post('/auth',loginUser);
+userRoutes.post('/logout',logCurrentUser);
 
 
+userRoutes
+  .route('/profile')
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate,updateCurrentUserProfile);
 export default userRoutes;
