e895a7b2694493e7c559e3d6586dae05edbe1767
akash-912 <akashdeep12896@gmail.com>
2025-12-23 21:21:55 +0530

    navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Outlet } from 'react-router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navigation from './pages/Auth/Navigation';
 
 const App = () => {
   return (
-    <div><h1 className="text-3xl font-bold underline text-blue-500">
-    Hello world!
-  </h1></div>
+    <>
+    <ToastContainer />
+    <Navigation />
+    <main className='py-3'>
+      <Outlet />
+    </main>
+    </>
   )
 }
 
