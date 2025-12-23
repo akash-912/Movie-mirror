1d423a8a48d551e79b7ae50f1fa355336eac7138
akash-912 <akashdeep12896@gmail.com>
2025-12-23 18:37:03 +0530

    redux

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {setupListeners} from '@reduxjs/toolkit/query'
+import { set } from "mongoose";
+
+const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) =>getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
+
+setupListeners(store.dispatch);
+
+export default store;
