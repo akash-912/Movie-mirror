25da827e3190631f0e44cb0b85ae80200b3bb040
akash-912 <akashdeep12896@gmail.com>
2025-12-29 12:31:50 +0530

    profile

--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -22,14 +22,60 @@ const Profile = () => {
     setEmail(userInfo.email);
 
   },[userInfo.email, userInfo.username]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if(password !== confirmPassword){
+      toast.error('Password do not match');
+    }
+    else{
+      try{
+        const res = await updateProfile({
+          _id: userInfo._id,
+          username,
+          email,
+          password,
+        }).unwrap();
+        dispatch(setCredentials({...res}));
+        toast.success('Profile Updated Successfully');
+      }catch (err){
+        toast.error(err?.data?.message || err.error);
+
+      }
+    }
+  }
   return (
     <div>
       <div className="container mx-auto p-4 mt-[10rem]">
         <div className='flex justify-center align-center md:flex md:space-x-4 '>
           <div className='md:w-1/3'>
             <h2 className='text-2xl font-semibold  mb-4'>
-              User Profile
+              Update Profile
             </h2>
+            <form onSubmit={submitHandler}>
+              <div className='mb-4'>
+                <label className='block text-white mb-2'>Username</label>
+                <input type='text' placeholder='Enter name' className='form-input p-4 rounded w-full ' value={username}  onChange={(e) => setUsername(e.target.value)}/>
+              </div>
+              <div className='mb-4'>
+                <label className='block text-white mb-2'>Email</label>
+                <input type='email' placeholder='Enter email' className='form-input p-4 rounded w-full ' value={email}  onChange={(e) => setEmail(e.target.value)}/>
+              </div>
+              <div className='mb-4'>
+                <label className='block text-white mb-2'>Password</label>
+                <input type='password' placeholder='Enter password' className='form-input p-4 rounded w-full ' value={password}  onChange={(e) => setPassword(e.target.value)}/>
+              </div>
+              <div className='mb-4'>
+                <label className='block text-white mb-2'>Confirm Password</label>
+                <input type='password' placeholder='Confirm password' className='form-input p-4 rounded w-full ' value={confirmPassword}  onChange={(e) => setConfirmPassword(e.target.value)}/>
+              </div>
+              <div className="flex justify-between">
+                <button type='submit' className='bg-teal-500 w-screen mt-[2rem] font-bold text-white py-2 px-4
+                rounded hover:bg-teal-600'>
+                  Update Profile
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
