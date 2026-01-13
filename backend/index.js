e132be5d79464adb98cd8482954c349eea43ec00
akash-912 <akashdeep12896@gmail.com>
2026-01-13 19:35:29 +0530

    upload image

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import genreRoutes from "./routes/genreRoutes.js";
 import moviesRoutes from "./routes/moviesRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 dotenv.config();
 connectDB();
 
@@ -24,5 +25,9 @@ const PORT = process.env.PORT || 3000;
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/genre', genreRoutes);
 app.use('/api/v1/movies',moviesRoutes);
+app.use('/api/v1/upload',uploadRoutes);
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname + '/uploads')))
 
 app.listen(PORT, ()=>console.log("Over System is running on "+PORT))
