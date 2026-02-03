840d6a8de48ad3b917a1bc638f010e956b8a1b02
akash-912 <akashdeep12896@gmail.com>
2026-02-03 23:13:07 +0530

    home page

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import {setupListeners} from '@reduxjs/toolkit/query'
 import authReducer from "./feature/auth/authSlice.js";
 import { apiSlice } from "./api/apiSlice.js";
+import moviesReducer from "../redux/feature/movies/moviesSlice.js"
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
+    movies: moviesReducer,
   },
   middleware: (getDefaultMiddleware) =>getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true
